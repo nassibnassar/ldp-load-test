6c20f8c85fb5665af3b4369fc813157efab18a04
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2018-12-15 18:49:32 -0500

    Load data to postgres

--- cmd/load/main.go
+++ cmd/load/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
 	"os"
 
+	"github.com/lib/pq"
 	_ "github.com/lib/pq"
 )
 
@@ -18,8 +20,20 @@ type Loan struct {
 }
 
 // LoadLoans reads JSON data extracted from mod-circulation-storage:
-// /loan-storage/loans.
-func LoadLoans(r io.Reader) error {
+// /loan-storage/loans and loads it to the specified LDP foliocore
+// database.
+func LoadLoans(r io.Reader, db *sql.DB) error {
+
+	txn, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	stmt, err := txn.Prepare(pq.CopyIn(
+		"loans", "id", "user_id", "loan_date"))
+	if err != nil {
+		return err
+	}
 
 	dec := json.NewDecoder(r)
 
@@ -39,15 +53,34 @@ func LoadLoans(r io.Reader) error {
 			return err
 		}
 
-		//fmt.Printf("%v | %v | %v\n", l.Id, l.UserId, l.LoanDate)
+		_, err = stmt.Exec(l.Id, l.UserId, l.LoanDate)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = stmt.Exec()
+	if err != nil {
+		return err
+	}
+
+	err = stmt.Close()
+	if err != nil {
+		return err
+	}
+
+	err = txn.Commit()
+	if err != nil {
+		return err
 	}
 
 	return nil
 }
 
 // LoadLoanFile reads a file containing JSON data extracted from
-// mod-circulation-storage: /loan-storage/loans.
-func LoadLoanFile(jsonFilename string) error {
+// mod-circulation-storage: /loan-storage/loans and loads it to the
+// specified LDP foliocore database.
+func LoadLoanFile(jsonFilename string, db *sql.DB) error {
 
 	jsonFile, err := os.Open(jsonFilename)
 	if err != nil {
@@ -55,12 +88,31 @@ func LoadLoanFile(jsonFilename string) error {
 	}
 	defer jsonFile.Close()
 
-	err = LoadLoans(jsonFile)
+	err = LoadLoans(jsonFile, db)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func OpenDatabase(host, port, user, password, dbname string) (*sql.DB, error) {
+
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s "+
+			"sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	// Ping the database to test for connection errors.
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
 
 func printError(err error) {
@@ -70,12 +122,20 @@ func printError(err error) {
 func main() {
 	sourceDir := "/Users/nassib/tmp/20181214_043055"
 
+	db, err := OpenDatabase(
+		"localhost", "5432", "okapi", "okapi25", "okapi")
+	if err != nil {
+		printError(err)
+		return
+	}
+	defer db.Close()
+
 	for x := 1; x <= 20; x++ {
 		jsonFilename := sourceDir +
 			fmt.Sprintf("/loan-storage.loans.json.%v", x)
 		fmt.Printf("%v\n", jsonFilename)
 
-		err := LoadLoanFile(jsonFilename)
+		err := LoadLoanFile(jsonFilename, db)
 		if err != nil {
 			printError(err)
 			return
